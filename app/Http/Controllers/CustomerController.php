e125f3b4206dda41366e1cc036b79e1a9bcbc613
Manish Yadav <manishyadav0012@gmail.com>
2019-09-07 14:00:28 +0530

    customer registration page created

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,31 +11,13 @@ class CustomerController extends BaseController
     /**
      * Funtion to return the view for invoice generation page
      */
-    public function invoiceView(Request $request)
+    public function addCustomer(Request $request)
     {
         try {
-            if (view()->exists('invoice-view')) {
-                $items = Item::all();
-
-                return view('invoice-view', ['items' => $items]);
+            if (view()->exists('add-customer')) {
+                return view('add-customer', ['items' => '']);
             } else {
-                return view('view-not-found', ['viewName' => 'Invoice page']);
-            }
-        } catch (\Throwable $th) {
-            throw $th;
-        }
-    }
-
-    /**
-     * Funtion to return the view for generate invoice
-     */
-    public function generateInvoice(Request $request)
-    {
-        try {
-            if (view()->exists('generate-invoice-view')) {
-
-            } else {
-                return view('view-not-found', ['viewName' => 'Generated Invoice page']);
+                return view('view-not-found', ['viewName' => 'Add Customer page']);
             }
         } catch (\Throwable $th) {
             throw $th;
---
+++ resources/views/add-customer.blade.php
@@ -0,0 +1,89 @@
+@extends('master')
+@section('page_heading','Invoice View')
+@section ('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Main content -->
+  <section class="">
+  <div class="page-wrapper bg-color-warapper p-t-180 p-b-100 font-robo">
+        <div class="cust-wrapper wrapper--w960">
+            <div class="card card-2">
+                <div class="card-heading"></div>
+                <div class="card-body">
+                    <h2 class="title">Customer Registration</h2>
+                    <form method="POST">
+                        <div class="input-group">
+                            <input class="input--style-2" type="text" placeholder="Name" name="name">
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-6">
+                                <div class="input-group">
+                                    <input class="input--style-2 js-datepicker" type="text" placeholder="Birthdate" name="birthday">
+                                    <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="input-group">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="gender">
+                                            <option disabled="disabled" selected="selected">Gender</option>
+                                            <option>Male</option>
+                                            <option>Female</option>
+                                            <option>Other</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-4">
+                                <div class="input-group">
+                                    <input class="input--style-2" type="text" placeholder="Mobile" name="mobile">
+                                </div>
+                            </div>
+                            <div class="col-8">
+                                <div class="input-group">
+                                    <input class="input--style-2" type="text" placeholder="Email" name="email">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-t-30">
+                            <button class="btn btn--radius btn--green" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
+  <!-- /.content -->
+</div>
+@endsection
+
+@section ('body_scripts')
+    <script src="{{ config('app.app_public_path') }}/vendor/datepicker/moment.min.js"></script>
+    <script src="{{ config('app.app_public_path') }}/vendor/datepicker/daterangepicker.js"></script>
+    <script src="{{ config('app.app_public_path') }}/vendor/select2/select2.min.js"></script>
+
+    <!-- Main JS-->
+    <script src="{{ config('app.app_public_path') }}js/add-customer.js"></script>
+@endsection
+
+@section ('styles')
+<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
+
+<!-- Icons font CSS-->
+    <link href="{{ config('app.app_public_path') }}/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="{{ config('app.app_public_path') }}/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="{{ config('app.app_public_path') }}/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="{{ config('app.app_public_path') }}/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link rel="stylesheet" href="{{ config('app.app_public_path') }}/css/add-customer.css"  media="all">
+@endsection

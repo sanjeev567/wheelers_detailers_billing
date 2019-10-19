ba1bfc90ffe375ad38b8c6b9a28626892853a878
Manish Yadav <manishyadav0012@gmail.com>
2019-10-19 08:23:13 +0530

    bug fix

--- config/app_config.php
+++ config/app_config.php
@@ -1,6 +1,5 @@
 <?php
 return [
-    'SELLER_GS_TIN' => '07AABCW6278Q1ZG',
     'SELLER_ADDRESS_LINE1' => 'A-10/64, Ground Floor, Opposite DPS',
     'SELLER_ADDRESS_LINE2' => 'Ghaziabad Gate No. 5 Site-3, Industrial Area Meerut Road',
     'SELLER_ADDRESS_LINE3' => 'Ghaziabad-201001',
